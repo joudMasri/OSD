57cf07ab8687408c88deb88510d4f9b33e00e669
Joud Masri <joudmasri679@gmail.com>
2022-08-24 11:24:56 +0300

    first'

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navLinks = document.querySelector('.nav-links');
+
+menuBtn.addEventListener('click', ()=>{
+    navLinks.classList.toggle('mobile-menu');
+})
+
+const navLogo = document.querySelector('.nav-logo');
+const navbar = document.querySelector('.navbar')
+const btnScrollToTop = document.getElementById("btnScrollToTop");
+
+window.onscroll =()=>{
+    if(window.pageYOffset >520){
+        btnScrollToTop.style.display ='flex';
+        // navLogo.style.display = 'block'
+        // navbar.style.position = 'absolute'
+        // btnScrollToTop.style.alignItems = 'center';
+
+        navbar.style.position = 'fixed';
+    }
+    else{
+        btnScrollToTop.style.display = 'none';
+        navbar.style.position = 'absolute'
+        // navLogo.style.display = 'none'
+    } 
+}
+
+btnScrollToTop.addEventListener("click",()=>{
+    // window.scrollTo(0,0);
+
+    // Using JQuery
+    $("html, body").animate({scrollTop: 0}, 200);
+});
+
+
+
+const mobileMenu = document.getElementById('mobile-menu');
+const x =document.querySelector('.hide-mobile-links');
+const mobileNav = document.querySelector('.mobile-nav');
+const logo = document.querySelector('.nav-logo');
+
+mobileMenu.addEventListener('click', ()=>{
+    mobileNav.style.display ='flex';
+    mobileMenu.style.display = 'none'
+    logo.style.display ='none'
+});
+
+x.addEventListener('click', ()=>{
+    mobileNav.style.display = 'none';
+    mobileMenu.style.display = 'flex';
+    logo.style.display ='block'
+});
---
+++ js/slickSlider.js
@@ -0,0 +1,50 @@
+// $('.slider').slick({
+//     dots: true,
+//     infinite: false,
+//     speed: 300,
+//     slidesToShow: 4,
+//     slidesToScroll: 4,
+//     responsive: [
+//       {
+//         breakpoint: 1024,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 3,
+//           infinite: true,
+//           dots: true
+//         }
+//       },
+//       {
+//         breakpoint: 600,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1
+//         }
+//       }
+//     ]
+//   });
+
+if($(document).width()>700){
+  $('.slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+  });
+}
+
+if($(document).width()<700){
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+  })
+}
